e2a8f1b5e9953ea521a9973ada20ad24d5d39f01
pacanada <pereirapcanada@gmail.com>
2023-04-25 20:31:24 +0200

    add readme

--- README.md
+++ README.md
@@ -1,1 +1,67 @@
 # codex-light
+
+Project to explore how to recreate a (super) light version of codex, only for python programming language
+
+## Quickstart
+
+```cmd
+python -m venv .venv
+// for windows .venv/Scripts/activate
+source .venv/bin/activate
+pip install -r requirements.txt
+```
+Run a 6MB transformer:
+```cmd
+python inference_v1_kaggle.py 
+```
+
+```
+Model has been loaded from True
+INPUT
+------------------
+
+            "Input values must either be float or int: " f"{list(locals().values())}"
+        )
+    projected_x = ((x * distance) / (z + distance)) * scale
+    projected_y = ((y * distance) / (z + di
+
+GENERATED
+------------------
+ff)))
+       with += np.splagocits(int, propes, dict):
+        for i i in (
+          ass _y] == exValse()
+
+
+if is_main_eximpleate():
+    "Hore Remural the namess for equal a imbe erroriations chautn of mat_positive
+    match m:
+        curromsitic_value data doctest
+    """"
+
+    return st(syprippe
+```
+Run a even lighter version (256 kB) with
+```
+python inference_v1.py 
+```
+```
+Model has been loaded from True
+INPUT
+------------------
+
+            "Input values must 
+
+GENERATED
+------------------
+=* soul
+   ""
+    febttw fllestp(secthirnct rate is (matep[0] * larst = [1] 4, 0:, "Slinvat(_cu, "+ stageme:
+     nreter: Checort ice rod weypals:
+         of nod mi Aremny)
+
+
+
+pr  imal the fountor preicke imn of ABo vetysfpacorn oul blis itestd clitr ecilopt
+    Lof inverurl_alstiate te meclution p
+```
---
+++ inference_v1.py
@@ -0,0 +1,46 @@
+from model import Transformer
+from utils import get_root, decode, encode
+from dataclasses import dataclass
+import torch
+#torch.manual_seed(1)
+@dataclass
+class Config:
+    block_size = 32
+    n_blocks = 5
+    epochs = int(1e4)
+    vocab_size = 187
+    embedding_dim = 12 # must be equal to head_size in this model but not in example
+    batch_size=128
+    evaluation_steps=100
+    n_head=6
+    learning_rate=0.001
+    dropout=0.1
+    load_model = True
+    path_model = get_root() / "weights/v1.pt"
+    def __post_init__(self):
+        if self.embedding_dim%self.n_head!=0:
+            raise ValueError(f"Embedding dimension {self.embedding_dim} should be a multiple of n_head={self.n_head}")
+config = Config()
+test = torch.load(get_root() / "data/test.pt")
+
+m = Transformer(config)
+
+if config.load_model:
+    try:
+        m.load_state_dict(torch.load(config.path_model, map_location=torch.device('cpu')))
+        m.eval()
+        print(f"Model has been loaded from {config.load_model}")
+    except:
+        print("Could not load model")
+
+input = test[:config.block_size]
+#input_ini = "MAIDER:\n\n Oh my lord!\n\nPABLO:\n\n"
+#input = (config.block_size-len(input_ini)-1)*" " + "\n" + input_ini
+#input = torch.tensor(data=encode(input), dtype=torch.long)
+
+print("INPUT")
+print("------------------")
+print(decode(input))
+print("\nGENERATED")
+print("------------------")
+print(decode(m.generate_sequence(input.view(1,-1), 300, config.block_size))[config.block_size:])
